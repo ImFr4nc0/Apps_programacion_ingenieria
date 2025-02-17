013bffd2b5463c31f7322813af03078ffd5d5a63
Alejandro Gómez Franco <124713643+ImFr4nc0@users.noreply.github.com>
2025-02-17 14:42:13 -0500

    Update datos_maderas.py

--- datos_maderas.py
+++ datos_maderas.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
 from io import StringIO
 
 def load_csv_from_url(url):
@@ -12,7 +13,7 @@ def load_csv_from_url(url):
         return None
 
 def main():
-    st.title("Cargar CSV en Streamlit")
+    st.title("Datos madera")
     
     option = st.radio("Selecciona la forma de carga:", ["Subir archivo", "Desde URL"])
     
@@ -36,6 +37,18 @@ def main():
         grouped_df = df.groupby(["DPTO", "ESPECIE"])["VOLUMEN M3"].sum().reset_index()
         sorted_df = grouped_df.sort_values(by=["DPTO", "VOLUMEN M3"], ascending=[True, False])
         st.dataframe(sorted_df)
+        
+        st.write("Top 10 especies con mayor volumen de madera:")
+        top_species = df.groupby("ESPECIE")["VOLUMEN M3"].sum().nlargest(10).reset_index()
+        
+        fig, ax = plt.subplots()
+        ax.barh(top_species["ESPECIE"], top_species["VOLUMEN M3"], color='skyblue')
+        ax.set_xlabel("Volumen M3")
+        ax.set_ylabel("Especie")
+        ax.set_title("Top 10 especies con mayor volumen de madera movilizado")
+        ax.invert_yaxis()
+        
+        st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
