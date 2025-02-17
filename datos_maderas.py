7541b39fe40694d72c97ea8eba345da60dfbe5e4
Alejandro Gómez Franco <124713643+ImFr4nc0@users.noreply.github.com>
2025-02-17 14:17:09 -0500

    Update datos_maderas.py

--- datos_maderas.py
+++ datos_maderas.py
@@ -1,43 +1,14 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyMrd0uqBaNXKaUYH44G/tAe"
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "-ZuR_eQVTtQo"
-      },
-      "outputs": [],
-      "source": [
-        "import streamlit as st\n",
-        "\n",
-        "# Título de la app\n",
-        "st.title(\"Mi primera app\")\n",
-        "\n",
-        "# Autor de la app\n",
-        "st.write(\"Esta app fue elaborada por COLOQUE AQUÍ SU NOMBRE.\")\n",
-        "\n",
-        "# Solicitar nombre del usuario\n",
-        "nombre_usuario = st.text_input(\"¿Cómo te llamas?\")\n",
-        "\n",
-        "# Mostrar mensaje de bienvenida\n",
-        "if nombre_usuario:\n",
-        "    st.write(f\"{nombre_usuario}, te doy la bienvenida a mi primera app.\")\n"
-      ]
-    }
-  ]
-}
+import streamlit as st
+
+# Título de la app
+st.title("Mi primera app")
+
+# Autor de la app
+st.write("Esta app fue elaborada por COLOQUE AQUÍ SU NOMBRE.")
+
+# Solicitar nombre del usuario
+nombre_usuario = st.text_input("¿Cómo te llamas?")
+
+# Mostrar mensaje de bienvenida
+if nombre_usuario:
+    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
